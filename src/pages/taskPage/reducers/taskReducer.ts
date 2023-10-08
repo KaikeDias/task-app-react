aea1727f7176ec285cd680e95b521a1a249b34f7
Kaike <kaikedias98@gmail.com>
2023-10-08 20:22:24 -0300

    feat: add taskReducer

---
+++ src/pages/taskPage/reducers/taskReducer.ts
@@ -0,0 +1,40 @@
+import Task from "../../../core/interfaces/task";
+
+interface TaskState {
+  tasks: Task[];
+}
+
+export enum ActionType {
+  ADDED,
+  CHANGED,
+  DELETED,
+}
+
+type ActionAdded = { type: ActionType.ADDED; payload: Task };
+type ActionChanged = { type: ActionType.CHANGED; payload: Task };
+type ActionDeleted = { type: ActionType.DELETED; payload: { id: number } };
+
+type Action = ActionAdded | ActionChanged | ActionDeleted;
+
+function tasksReducer(state: TaskState, action: Action): TaskState {
+  switch (action.type) {
+    case ActionType.ADDED: {
+      return { tasks: [...state.tasks, action.payload] };
+    }
+    case ActionType.CHANGED: {
+      return {
+        tasks: state.tasks.map((task) =>
+          task.id === action.payload.id ? action.payload : task
+        ),
+      };
+    }
+    case ActionType.DELETED: {
+      return {
+        tasks: state.tasks.filter((task) => task.id !== action.payload.id),
+      };
+    }
+    default: {
+      throw Error("Unknown action");
+    }
+  }
+}
