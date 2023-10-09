a98ee37d2b1d6957705a2df2b28ab1e74bf7af53
Kaike <kaikedias98@gmail.com>
2023-10-08 22:22:52 -0300

    refactor: remove unused imports and functions

--- src/pages/taskPage/index.tsx
+++ src/pages/taskPage/index.tsx
@@ -1,9 +1,8 @@
 import { useReducer } from "react";
-import { ActionType, TaskState, tasksReducer } from "./reducers/taskReducer";
+import { TaskState, tasksReducer } from "./reducers/taskReducer";
 import Task from "../../core/interfaces/task";
 import TaskList from "./components/taskList";
 import { TasksContext, TasksDispatchContext } from "./context/taskContext";
-import { getNextId} from "./utils/nextId";
 import AddTask from "./components/addTask";
 
 const initialTasks: Task[] = [
@@ -17,31 +16,6 @@ const initialTasksState: TaskState = { tasks: initialTasks };
 
 export default function TaskPage() {
   const [tasks, dispatch] = useReducer(tasksReducer, initialTasksState);
-
-  const handleAddTask = (text: string) => {
-    dispatch({
-      type: ActionType.ADDED,
-      payload: {
-        id: getNextId(),
-        text: text,
-        isDone: false,
-      },
-    });
-  };
-
-  const handleChangeTask = (task: Task) => {
-    dispatch({
-      type: ActionType.CHANGED,
-      payload: task,
-    });
-  };
-
-  const handleDeleteTask = (taskId: number) => {
-    dispatch({
-      type: ActionType.DELETED,
-      payload: { id: taskId },
-    });
-  };
 
   return (
     <TasksContext.Provider value={tasks.tasks}>
